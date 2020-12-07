641f274c46c0526191bc47726a0879fd4eeaac1d
mogrim-91 <66732473+mogrim-91@users.noreply.github.com>
2020-12-07 23:53:00 +0100

    clickTest

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,4 +1,12 @@
 /* Click */
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  window.alert('I feel much better now! Tack så mycket.😋');
+});
+
+/* Can I loop out different prompt messages?
+Should I be giving the buttons class names to make the queries easier?*/
 
 /*Keypress*/
 
