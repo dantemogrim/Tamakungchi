23683d396a1e993b720bc34d3bb2d368b04ba817
mogrim-91 <66732473+mogrim-91@users.noreply.github.com>
2020-12-09 23:42:58 +0100

    AudioAdjustmentsAndLoop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
-/* Click */
-const button = document.querySelector('button');
+/* Is it possible to loop these out in my feedButton click Event? */
 
-button.addEventListener('click', () => {
+const quotes = [
+  'Tack för maten, sa soldaten, jag vill inte diska faten.',
+  'Det kommer inga stekta sparvar flygande om du inte själv bemödar dig om att göra ditt bästa',
+  'Ämnet för dagens varmrätt är brännskador.',
+  'Härmed förklarar jag den nya potatisgratängen invigd som ska ätas här mellan.. eh.. tuggorna.',
+  'Vad är det här för rätt? Små-Tarzans med Allan-syndrom?',
+];
+
+/* Feed button with window alert event. */
+const feedButton = document.getElementById('feed');
+
+feedButton.addEventListener('click', () => {
   window.alert('Tack för maten, sa soldaten, jag vill inte diska faten!');
 });
 
-/* Playground area. */
+/* Play button with play audio event. */
 
 let tamagotchiSong = document.getElementById('music');
 function play() {
   tamagotchiSong.play();
 }
-
-/* End of playground area. */
