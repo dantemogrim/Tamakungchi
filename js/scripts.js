06e4754a8a498103f7841ee4d60437e9ff193d6b
mogrim-91 <66732473+mogrim-91@users.noreply.github.com>
2020-12-09 15:56:21 +0100

    playButton

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,11 @@ button.addEventListener('click', () => {
   window.alert('Tack för maten, sa soldaten, jag vill inte diska faten!');
 });
 
-document.getElementById('feed').addEventListener(
-  'click',
-  function () {
-    click1(1);
-  },
-  false
-);
-// document.getElementById('feed').innerHTML = 'Om-nom!';
+/* Playground area. */
 
-/* Can I loop out different prompt messages?
-Should I be giving the buttons class names to make the queries easier?*/
+let tamagotchiSong = document.getElementById('music');
+function play() {
+  tamagotchiSong.play();
+}
 
-/*Keypress*/
-
-// window.addEventListener('keypress', function (event) {
-//   console.log("You've pressed the key:", event.key);
-// });
-
-/* Scroll */
-
-// window.addEventListener('scroll', function (e) {
-//   console.log('log scroll from top:', window.scrollY);
-// });
+/* End of playground area. */
