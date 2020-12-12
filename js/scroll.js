c991e13c4c8d26373756a0fd251903ca72f5ab3a
mogrim-91 <66732473+mogrim-91@users.noreply.github.com>
2020-12-12 20:04:50 +0100

    scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
 //KEYPRESS IS OUTDATED ACCORDING TO MOZILLA
 //TRY KEYDOWN OR KEYUP OR SMTH ELSE INSTEAD
 //PERHAPS TRY AND MAKE A 'RESET' THAT GOES BEEP BEEP
+//SCROLL BEEP BEEP!!!!!!!
 
 let resetBtn = window.addEventListener('keypress', function (event) {
   console.log("You've pressed the key:", event.key);
