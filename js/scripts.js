df9f621b35da9f99639a9b6c807c8849a070a7ab
mogrim-91 <66732473+mogrim-91@users.noreply.github.com>
2020-12-12 13:31:44 +0100

    Revert "gettingthatplaybuttongoing"
    This reverts commit 584355f5c337b6a02da11a4fc80e325d7038b59d.

--- js/scripts.js
+++ js/scripts.js
@@ -22,18 +22,23 @@ const coolSongs = [
   '../audio/test2.mp3',
 ];
 
-// Grabbing the button element.
-const playBtn = document.getElementById('play');
+/*PLAYGROUND TEST SITE */
 
-// Assigning addEventListener click event to the button element.
-playBtn.addEventListener('click', () => {
-  // Randomizes the songs and assigns it to the variable.
-  const oneSong = coolSongs[Math.floor(Math.random() * coolSongs.length)];
+// const playButton = document.querySelector('#play');
+// const audioElement = document.querySelector('#music');
 
-  // Creating Audio file and assigning it to a variable.
-  const tamagotchiSong = new Audio();
-  // Assigning the path to the song.
-  tamagotchiSong.src = oneSong;
-  // Let's play it.
-  tamagotchiSong.play();
+// playButton.addEventListener('click', () => {
+//   const coolSong = coolSongs[Math.floor(Math.random() * coolSongs.length)];
+//   audio.src = `${coolSong}`;
+//   console.log(coolSong);
+//   play(coolSong);
+// });
+
+/* */
+
+const audioElement = document.getElementById('music');
+
+audioElement.addEventListener('click', () => {
+  const coolSong = coolSongs[Math.floor(Math.random() * coolSongs.length)];
+  audio.play(coolSong);
 });
