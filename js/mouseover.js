2869d41f09726ed0691ee197f0117eb89632e217
mogrim-91 <66732473+mogrim-91@users.noreply.github.com>
2020-12-14 23:27:23 +0100

    css

--- js/mouseover.js
+++ js/mouseover.js
@@ -1,17 +1,17 @@
 'use strict';
 // A mouseover event that swaps the main gif on the site.
 
-const helloKingen = document.querySelector('#gifContainer');
+const theKingGifs = document.querySelector('#gifContainer');
 
 const image = document.querySelector('#gifContainer img');
 
-helloKingen.addEventListener('mouseover', () => {
+theKingGifs.addEventListener('mouseover', () => {
   image.src = 'media/dancing-king.gif';
   image.alt =
     'The Swedish king on the dancefloor. Dancing, shooting his chest out and back.';
 });
 
-helloKingen.addEventListener('mouseleave', () => {
+theKingGifs.addEventListener('mouseleave', () => {
   image.src = 'media/king-smirk.gif';
   image.alt = 'The Swedish king in a tuxedo, smirking and wobbling his head.';
 });
